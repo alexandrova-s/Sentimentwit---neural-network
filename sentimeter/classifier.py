c3384691cd0ff1c11d90fcc5693a9f33708190ed
Aleksandra <ale.szczesniak06@gmail.com>
2019-11-23 14:59:10 +0100

    Create classifier.py

---
+++ sentimeter/classifier.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class Classifier:
+
+    def __init__(self):
+        pass
+
+    def classify(self, y: np.ndarray) -> int:
+        assert y.ndim == 1
+        assert False, "Not implemented"
